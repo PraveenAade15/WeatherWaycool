26f3088398c6a68b9cd7a606a4744ef1b1c4290a
macos <aadepraveen15@gmail.com>
2022-07-13 21:55:53 +0530

    weather app waycool

---
+++ app/src/main/java/com/example/waycoolweatherapp/di/RepositoryModule.kt
@@ -0,0 +1,23 @@
+package com.example.waycoolweatherapp.di
+
+
+import com.example.waycoolweatherapp.data.repository.WeatherRepositoryImpl
+import com.example.waycoolweatherapp.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl
+    ): WeatherRepository
+}
